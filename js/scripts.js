67792787ee22ff27251f040a8a7441da0ed7194b
Ivan Micai <ivanmicai@gmail.com>
2016-02-17 17:31:35 -0200

    Apply JSHint

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
+/* global $ YPlaylist */
+
 $(function() {
+  'use strict';
 
   var $mediasection = $('.media-section');
   var $mainmedia = $mediasection.children('.main-media');
@@ -7,13 +10,15 @@ $(function() {
   // navegacao de midia
 
   $mediasection.each(function(i) {
-    $mediasection.eq(i).addClass('main-section-'+i);
-  })
+    $mediasection.eq(i).addClass('main-section-' + i);
+  });
 
   function mediaClick() {
-    $mediathumbs.on('click', '.media-thumb', function(e) {
-      el = $(this);
-      galleryIndex = el.parent($mainmedia).parent($mediasection).attr('class').replace("row media-section main-section-", "");
+    $mediathumbs.on('click', '.media-thumb', function() {
+      var el = $(this);
+      var mediaToLoad;
+      var galleryIndex = el.parent($mainmedia).parent($mediasection).attr('class').replace('row media-section main-section-', '');
+      var desc;
       $mediathumbs.children('.media-thumb').removeClass('active');
       el.addClass('active');
 
@@ -21,21 +26,21 @@ $(function() {
 
         mediaToLoad = el.children('img').attr('data-url');
         desc = el.attr('title');
-        $mediasection.eq(galleryIndex).children('.main-media').empty().append('<div class="insta-photo"><img src="'+mediaToLoad+'"></div><div class="insta-desc">'+desc+'</div>');
+        $mediasection.eq(galleryIndex).children('.main-media').empty().append('<div class="insta-photo"><img src="' + mediaToLoad + '"></div><div class="insta-desc">' + desc + '</div>');
 
       }
 
       if (el.children('img').hasClass('media-youtube')) {
-        mediaToLoad = el.children('img').attr('data-url').replace("https://www.youtube.com/watch?v=", "");
-        $mediasection.eq(galleryIndex).children('.main-media').empty().append('<iframe width="100%" height="480" src="https://www.youtube.com/embed/'+mediaToLoad+'" frameborder="0" allowfullscreen></iframe>');
+        mediaToLoad = el.children('img').attr('data-url').replace('https://www.youtube.com/watch?v=', '');
+        $mediasection.eq(galleryIndex).children('.main-media').empty().append('<iframe width="100%" height="480" src="https://www.youtube.com/embed/' + mediaToLoad + '" frameborder="0" allowfullscreen></iframe>');
       }
 
       if (el.children('img').hasClass('media-vimeo')) {
-        mediaToLoad = el.children('img').attr('data-url').replace("https://vimeo.com/", "");
-        $mediasection.eq(galleryIndex).children('.main-media').empty().append('<iframe src="https://player.vimeo.com/video/'+mediaToLoad+'" width="100%" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        mediaToLoad = el.children('img').attr('data-url').replace('https://vimeo.com/', '');
+        $mediasection.eq(galleryIndex).children('.main-media').empty().append('<iframe src="https://player.vimeo.com/video/' + mediaToLoad + '" width="100%" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
       }
 
-    })
+    });
 
   }
   mediaClick();
@@ -47,13 +52,13 @@ $(function() {
       .attr('data-url', photo.images.standard_resolution.url)
       .attr('src', photo.images.thumbnail.url);
 
-    html = $('<div>')
+    var html = $('<div>')
       .addClass('media-thumb')
       .attr('id', photo.id)
       .attr('title', photo.caption.text)
       .append(innerHtml);
 
-    if (active == true) {
+    if (active === true) {
       html.addClass('active');
     }
 
@@ -62,12 +67,13 @@ $(function() {
 
   $('#instagram .media-thumbs').on('didLoadInstagram', function(event, response) {
     var that = this;
-    $('#instagram .main-media').append('<div class="insta-photo"><img src="'+response.data[0].images.standard_resolution.url+'"></div><div class="insta-desc">'+response.data[0].caption.text+'</div>');
+    $('#instagram .main-media').append('<div class="insta-photo"><img src="' + response.data[0].images.standard_resolution.url + '"></div><div class="insta-desc">' + response.data[0].caption.text + '</div>');
     $.each(response.data, function(i, photo) {
-      if (i == 0)
-        $(that).append(createPhotoElement(photo, true))
-      else
+      if (i === 0){
+        $(that).append(createPhotoElement(photo, true));
+      }else{
         $(that).append(createPhotoElement(photo, false));
+      }
     });
     //mediaClick();
   });
@@ -86,7 +92,7 @@ $(function() {
 
 
   $('[data-toggle="tooltip"]').tooltip();
-  $mainmenu = $('#main-menu');
+  var $mobilemainmenu, $sociallinks, $mainmenu = $('#main-menu');
 
   setTimeout(function() {
     $('.opening-passo-1').removeClass('fadeInDown').addClass('fadeOut');
@@ -106,12 +112,12 @@ $(function() {
           'LGBT',
           'indígenas',
           'pop.rua',
-          'tod@s',
+          'tod@s'
         ]);
         setTimeout(function() {
           //$(document).scrollTop( $("#main-menu-section").offset().top )
-          $('body, html').animate({scrollTop: $("#main-menu-section").offset().top}, 400);
-        }, 8000)
+          $('body, html').animate({scrollTop: $('#main-menu-section').offset().top}, 400);
+        }, 8000);
       }, 600);
 
     }, 1500);
@@ -130,46 +136,46 @@ $(function() {
 
   $('#conexoes').waypoint({
     handler: function(direction) {
-      if (direction == "down" && biggraficocomplete == 0) {
+      if (direction === 'down' && biggraficocomplete === 0) {
         biggrafico1.drawsvg('animate');
         biggrafico2.drawsvg('animate');
         biggrafico3.drawsvg('animate');
         biggraficocomplete = 1;
       }
     }
-  })
+  });
 
   $('#instasvg-trigger').waypoint({
     handler: function(direction) {
-      if (direction == "down" && instasvgcomplete == 0) {
+      if (direction === 'down' && instasvgcomplete === 0) {
         instasvg.drawsvg('animate');
         instasvgcomplete = 1;
       }
     }
-  })
+  });
 
   $('#susconectahashs-trigger').waypoint({
     handler: function(direction) {
-      if (direction == "down" && susconectahashscomplete == 0) {
+      if (direction === 'down' && susconectahashscomplete === 0) {
         susconectahashs.drawsvg('animate');
         susconectalinhas.drawsvg('animate');
         susconectahashscomplete = 1;
       }
     }
-  })
+  });
 
   // define a altura minima das seções principais
-  $('body > section').not('.two').css('min-height',  window.innerHeight+'px');
+  $('body > section').not('.two').css('min-height', window.innerHeight + 'px');
   // menu affix
   $('#main-menu-section').affix({
     offset: {
       top: window.innerHeight,
       bottom: 0
     }
-  })
+  });
 
   if (window.innerHeight < 980) {
-    $mainmenu.addClass("mobile");
+    $mainmenu.addClass('mobile');
   } else {
     $mainmenu.removeClass('mobile');
   }
@@ -178,14 +184,14 @@ $(function() {
   $(window).resize(function() {
 
     if (window.innerHeight < 980) {
-      $mainmenu.addClass("mobile");
+      $mainmenu.addClass('mobile');
       $mobilemainmenu = $('#main-menu.mobile');
     } else {
       $mainmenu.removeClass('mobile');
     }
 
     // define a altura minima das seções principais
-    $('body > section').not('.two').css('min-height',  window.innerHeight+'px');
+    $('body > section').not('.two').css('min-height', window.innerHeight + 'px');
     // menu affix
     $('#main-menu-section').data('bs.affix').options.offset.top = window.innerHeight;
     $('#main-menu-section').data('bs.affix').options.offset.bottom = 0;
@@ -199,25 +205,25 @@ $(function() {
     if (el.hasClass('slider')) {
       return;
     }
-    $("#main-menu ul li").removeClass('active');
+    $('#main-menu ul li').removeClass('active');
     el.addClass('active');
     var whatTab = el.index();
     var howFar = 0;
     if (whatTab > 0) {
-      howFar += 4*whatTab;
+      howFar += 4 * whatTab;
       for (var i = 0; i < whatTab; i++) {
-        howFar += $("#main-menu ul li").eq(i).width();
+        howFar += $('#main-menu ul li').eq(i).width();
       }
     }
-    $(".slider").css({
-      left: (howFar + parseInt(el.children('a').css('padding-left')) ) + "px",
-      width:  el.children('a').width() + "px"
+    $('.slider').css({
+      left: (howFar + parseInt(el.children('a').css('padding-left')) ) + 'px',
+      width: el.children('a').width() + 'px'
     });
 
-    var posX = el.offset().left,
-        posY = el.offset().top,
-        buttonWidth = el.width(),
+    var buttonWidth = el.width(),
         buttonHeight = el.height();
+        //posX = el.offset().left,
+        //posY = el.offset().top,
 
     if (buttonWidth >= buttonHeight) {
       buttonHeight = buttonWidth;
@@ -230,48 +236,48 @@ $(function() {
   }
 
   // aplica o efeito no menu no clique
-  $("#main-menu ul li").click(function(e) {
+  $('#main-menu ul li').click(function() {
     menuClick($(this));
   });
   // aplica o efeito do menu no scroll
-  $('#main-menu').on('activate.bs.scrollspy', function (e) {
+  $('#main-menu').on('activate.bs.scrollspy', function () {
 
-    menuClick($("#main-menu ul li.active"));
+    menuClick($('#main-menu ul li.active'));
 
-  })
+  });
 
   // smooth scroll
-  $('a[href^="#"]').not('.no-scroll').not('.collapsed').on('click',function (e) {
-	    e.preventDefault();
+  $('a[href^="#"]').not('.no-scroll').not('.collapsed').on('click', function (e) {
+      e.preventDefault();
 
-	    var target = this.hash;
-	    var $target = $(target);
+      var target = this.hash;
+      var $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
 
   // animando os botões de links social-links
   $sociallinks = $('ul.social-links li img');
   $sociallinks.on('mouseover', function() {
     $(this).addClass('infinite pulse animated');
-  })
+  });
   $sociallinks.on('mouseout', function() {
     $(this).removeClass('infinite pulse animated');
-  })
+  });
 
   if (window.innerHeight < 980) {
     $('.hamburg-menu').on('click', function() {
-      $(this).toggleClass("active");
+      $(this).toggleClass('active');
       $mobilemainmenu.children('ul').slideToggle('fast');
-    })
+    });
     $mainmenu.children('ul').children('li').on('click', function() {
       $mobilemainmenu.children('ul').slideToggle('fast');
       $('.hamburg-menu').removeClass('active');
-    })
+    });
   }
 
-})
+});
